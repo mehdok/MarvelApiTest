6b23886aedd6a09aecd9a7ad5bf97745dae1ced8
Mehdi Sohrabi <mehdok@gmail.com>
2020-10-29 10:46:06 +0330

    Add PluggableApplicationDelegate;

--- MarvelApiTest/AppDelegate.swift
+++ MarvelApiTest/AppDelegate.swift
@@ -8,30 +8,11 @@
 import UIKit
 import RxSwift
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    public var window: UIWindow?
-
-    private var appCoordinator: AppCoordinator!
-    // TODO: dispose bag when app is terminated
-    private let disposeBag = DisposeBag()
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        startFirstPage()
-        return true
-    }
-
-}
-
-extension AppDelegate {
-    func startFirstPage() {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        appCoordinator = AppCoordinator(window: window!)
-        appCoordinator.start()
-            .subscribe()
-            .disposed(by: disposeBag)
+@UIApplicationMain
+class AppDelegate: PluggableApplicationDelegate {
+    override func services() -> [ApplicationService] {
+        [
+            UIApplicationService(window: window),
+        ]
     }
 }
-
-
