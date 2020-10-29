b5537bb3c1419cc40564f8010baf4e59b38243da
Mehdi Sohrabi <mehdok@gmail.com>
2020-10-29 17:09:31 +0330

    Add Dependency injection to the project;

--- MarvelApiTest/AppDelegate.swift
+++ MarvelApiTest/AppDelegate.swift
@@ -5,13 +5,14 @@
 //  Created by Mehdok on 10/29/20.
 //
 
-import UIKit
 import RxSwift
+import UIKit
 
 @UIApplicationMain
 class AppDelegate: PluggableApplicationDelegate {
     override func services() -> [ApplicationService] {
         [
+            DependenciesApplicationService(),
             UIApplicationService(window: window),
         ]
     }
