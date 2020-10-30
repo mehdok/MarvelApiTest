faac248597193e3bbcc8130d7e7133a4977ea5d9
Mehdi Sohrabi <mehdok@gmail.com>
2020-10-30 15:29:47 +0330

    Open detail screen by tapping on each character;

--- MarvelApiTest/Screens/MainScreen/MainScreenCR.swift
+++ MarvelApiTest/Screens/MainScreen/MainScreenCR.swift
@@ -8,6 +8,7 @@
 import RxSwift
 import DataLayer
 import DomainLayer
+import RxCocoa
 
 class MainScreenCR: BaseCoordinator<Void> {
     @Inject private var dataModule: DataModuleType
@@ -20,12 +21,30 @@ class MainScreenCR: BaseCoordinator<Void> {
     
     override func start() -> Observable<Void> {
         let viewController = MainScreenVC.instance()
+        
         let viewModel = MainScreenVM(marvelCharactersUsecase: dataModule.component())
         viewModel.bindViewDidLoad(viewController.rx.viewDidLoad.asDriver())
+        // handle opening detail screen, triggered from viewModel Observable
+        viewModel.didOpenDetailScreen
+            .map { [unowned self] in (self.navigationController, $0) }
+            .bind(to: rx_openDetailScreen)
+            .disposed(by: bag)
         viewController.viewModel = viewModel
 
         navigationController.viewControllers = [viewController]
 
         return Observable.never()
+    }
+}
+
+extension MainScreenCR {
+    public var rx_openDetailScreen: AnyObserver<(UINavigationController, Character)> {
+        return Binder(self, binding: { [unowned self] _, data in
+            let (root, character) = data
+            let coordinator = DetailScreenCR(rootNavigation: root, character: character)
+            self.coordinate(to: coordinator)
+                // subscribe and dispose to let go of coordinator when it returns
+                .subscribe { _ in }.disposed(by: self.bag)
+        }).asObserver()
     }
 }
