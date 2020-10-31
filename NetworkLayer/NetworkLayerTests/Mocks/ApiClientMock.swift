0b44ea895c109fe5f5b05b3dbab6d7ee5fc80a9a
Mehdi Sohrabi <mehdok@gmail.com>
2020-10-31 11:37:16 +0330

    Add unit test for marvel character repository;

---
+++ NetworkLayer/NetworkLayerTests/Mocks/ApiClientMock.swift
@@ -0,0 +1,12 @@
+//
+//  ApiClientMock.swift
+//  NetworkLayerTests
+//
+//  Created by Mehdok on 10/31/20.
+//
+
+import UIKit
+
+class ApiClientMock: NSObject {
+
+}
---
+++ NetworkLayer/NetworkLayerTests/Mocks/EndPointExt.swift
@@ -0,0 +1,20 @@
+//
+//  EndPointExt.swift
+//  NetworkLayerTests
+//
+//  Created by Mehdok on 10/31/20.
+//
+
+import UIKit
+
+class EndPointExt: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
