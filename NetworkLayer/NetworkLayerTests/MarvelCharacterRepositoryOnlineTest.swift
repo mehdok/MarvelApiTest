b013d97ca68d3ed883cce2a468a3dbaa79e9208d
Mehdi Sohrabi <mehdok@gmail.com>
2020-10-31 11:37:38 +0330

    Add unit test for marvel character repository;

--- NetworkLayer/NetworkLayerTests/MarvelCharacterRepositoryOnlineTest.swift
+++ NetworkLayer/NetworkLayerTests/MarvelCharacterRepositoryOnlineTest.swift
@@ -5,8 +5,36 @@
 //  Created by Mehdok on 10/31/20.
 //
 
-import UIKit
+import DomainLayer
+@testable import NetworkLayer
+import RxBlocking
+import RxSwift
+import XCTest
 
-class MarvelCharacterRepositoryOnlineTest: NSObject {
+class MarvelCharacterRepositoryOnlineTest: XCTestCase {
+    var repository: MarvelCharacterRepositoryOnline!
+    
+    override func setUp() {
+        super.setUp()
+        repository = MarvelCharacterRepositoryOnlineImpl(client: ApiClientMock())
+    }
+    
+    func testReceivedData() throws {
+        let result = try repository.getCharacters(limit: 0, offset: 0) .toBlocking().first()
+
+        XCTAssertNotNil(result)
+
+        switch result!.status {
+        case .success:
+            XCTAssertNotNil(result?.data)
+            let characters = (result!.data!.data?.results)!
+            XCTAssertTrue(type(of: characters) == [Character].self)
+            XCTAssertEqual(characters.count, 20)
+            XCTAssertEqual(characters.first!.name, "3-D Man")
+        case .failure:
+            XCTAssertNotNil(result!.error)
+            XCTAssertTrue(type(of: result!.error!) == MarvelError.self)
+        }
+    }
 
 }
--- NetworkLayer/NetworkLayerTests/Mocks/EndPointExt.swift
+++ NetworkLayer/NetworkLayerTests/Mocks/EndPointExt.swift
@@ -5,16 +5,13 @@
 //  Created by Mehdok on 10/31/20.
 //
 
-import UIKit
+@testable import NetworkLayer
 
-class EndPointExt: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+extension EndPoint {
+    var mockJsonFile: String {
+        switch self {
+        case .characters:
+            return "CharactersData"
+        }
     }
-    */
-
 }
